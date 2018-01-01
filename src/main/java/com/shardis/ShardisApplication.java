bd151021186073be5919cc4aec349198e4447318
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2018-01-01 14:28:58 +0100

    add empty spring-boot project

---
+++ src/main/java/com/shardis/ShardisApplication.java
@@ -0,0 +1,12 @@
+package com.shardis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardisApplication.class, args);
+	}
+}
