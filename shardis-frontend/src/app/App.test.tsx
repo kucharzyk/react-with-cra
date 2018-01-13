5defb497cdef8c17313d28a2c51345c42f317959
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2018-01-13 19:06:11 +0100

    update react-scripts-ts

--- shardis-frontend/src/app/App.test.tsx
+++ shardis-frontend/src/app/App.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import * as fetchMock from 'fetch-mock'
+import * as fetchMock from 'fetch-mock';
 
 describe('App', () => {
 
