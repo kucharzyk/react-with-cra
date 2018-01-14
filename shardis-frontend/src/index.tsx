79a57b99b354713a561000ac8c9d556c22cef02e
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2018-01-14 11:21:15 +0100

    add roboto font (multilang)

--- shardis-frontend/src/index.tsx
+++ shardis-frontend/src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import 'typeface-roboto-multilang/latin-ext.css';
 import App from './app/App';
 
 ReactDOM.render(
