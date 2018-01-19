708b7ceabfde0368ab6744d7a6a6d8802046fe45
kucharzyk.tomasz <kucharzyk.tomasz@gmail.com>
2018-01-19 19:18:50 +0100

    add redux-logger

--- shardis-frontend/src/store.ts
+++ shardis-frontend/src/store.ts
@@ -2,9 +2,21 @@ import { applyMiddleware, createStore, Middleware } from 'redux';
 import StoreState from './types/StoreState';
 import reducer from './reducer';
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import { isDev } from './env';
+import { createLogger } from 'redux-logger';
 
 const getMiddleware: (() => Middleware[]) = () => {
-  return [];
+  if (isDev) {
+    const logger = createLogger({
+      diff: true,
+      collapsed: true
+    });
+    return [
+      logger
+    ];
+  } else {
+    return [];
+  }
 };
 
 const store = createStore<StoreState>(reducer, composeWithDevTools(applyMiddleware(...getMiddleware())));
